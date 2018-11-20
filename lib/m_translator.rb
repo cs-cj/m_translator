8654cd7e651ba040f68902f86a135b374da02a7c
jack <qinshenyong@socialdatamax.com>
2018-11-20 17:34:06 +0800

    fix

--- lib/m_translator.rb
+++ lib/m_translator.rb
@@ -2,9 +2,8 @@ require 'rest-client'
 require 'json'
 require 'execjs'
 require 'active_support/all'
-require "m_translator/version"
-require 'm_translator/youdao'
-require 'm_translator/google'
+
+Dir[File.dirname(__FILE__) + '/m_translator/*.rb'].each {|file| require file }
 
 module MTranslator
   class Error < StandardError; end
